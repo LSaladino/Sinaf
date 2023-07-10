bcd22e63f5f003dd4654de42732bdb30e7c95982
LSaladino <luis.saladino@gmail.com>
2023-07-10 08:49:00 -0300

    commit

---
+++ sinaf-api/Migrations/20230710105654_InitMigra.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SinafApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitMigra : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Boletos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    NomeCliente = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Valor = table.Column<double>(type: "float", nullable: false),
+                    LinhaDigitavel = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Boletos", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "MeioPagamentos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    NomeCliente = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Valor = table.Column<double>(type: "float", nullable: false),
+                    LinhaDigitavel = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    QRCode = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    TipoPagamento = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Processado = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MeioPagamentos", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Pixes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    NomeCliente = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Valor = table.Column<double>(type: "float", nullable: false),
+                    QRCode = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pixes", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Boletos");
+
+            migrationBuilder.DropTable(
+                name: "MeioPagamentos");
+
+            migrationBuilder.DropTable(
+                name: "Pixes");
+        }
+    }
+}
---
+++ sinaf-api/Program.cs
@@ -0,0 +1,46 @@
+using Microsoft.EntityFrameworkCore;
+using SinafApi.Data;
+using SinafApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers()
+// Microsoft.AspNetCore.Mvc.NewtonsoftJson
+.AddNewtonsoftJson(opt => opt.SerializerSettings.ReferenceLoopHandling = 
+Newtonsoft.Json.ReferenceLoopHandling.Ignore);
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// ADD FOR ME INJECTION DEPENCY HERE TO DONNES PROJECT
+builder.Services.AddScoped<IRepository, Repository>();
+
+//ADD FOR ME CONTEXT ACCESS TO DONNES PROJECT
+builder.Services.AddDbContext<MyDataContext>(opt => opt.UseSqlServer(builder.Configuration.GetConnectionString("Conn")));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+// app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+// ADD CORS POLITICS
+app.UseCors(c => c.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin());
+
+// app.UseDefaultFiles();
+// app.UseStaticFiles();
+
+
+app.Run();
